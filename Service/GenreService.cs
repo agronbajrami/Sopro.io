df2d00324e393f4aa0cddb93eec5ec15500d74cf
Agron Bajrami <60615745+agronbajrami@users.noreply.github.com>
2022-05-09 12:06:04 +0200

    Add files via upload

---
+++ Service/GenreService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Domain.Interface.Repository;
+using Domain.Interface.Service;
+using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class GenreService : IGenreService
+    {
+        private readonly IGenreRepository _genreRepository;
+        private readonly IMapper _mapper;
+        public GenreService(IGenreRepository genreRepository, IMapper mapper)
+        {
+            _genreRepository = genreRepository;
+            _mapper = mapper;
+        }
+      
+        public async Task<List<GenreClass>> GetAll()
+        {
+            var result = await _genreRepository.GetAll();
+            return result;
+        }
+        public async Task Create(GenreModel genreModel)
+        {
+            var mappedMovie = new GenreClass()
+            {
+                Name = genreModel.Name
+            };
+            await _genreRepository.Create(mappedMovie);
+        }
+        public async Task<GenreClass> GetGenre(int id)
+        {
+            var result = await _genreRepository.GetGenre(id);
+            return result;
+        }
+
+    
+    }
+}
---
+++ Service/MovieService.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Domain.Interface.Repository;
+using Domain.Interface.Service;
+using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _movieRepository;
+        private readonly IGenreRepository _genreRepository;
+        private readonly IPersonRepository _personRepository;
+        private readonly IMapper _mapper;
+        public MovieService(IMovieRepository movieRepository, IMapper mapper , IGenreRepository genreRepository, IPersonRepository personRepository)
+        {
+            _movieRepository = movieRepository;
+            _mapper = mapper;
+            _genreRepository = genreRepository;
+            _personRepository = personRepository;
+        }
+        public async Task Create(MovieModel movieModel)
+        {
+            var mappedMovie = new Movie()
+            {
+                Description = movieModel.Description,
+                LengthInMin = movieModel.LengthInMin,
+                Name = movieModel.Name,
+                ReleaseTime = movieModel.ReleaseTime
+            };
+            mappedMovie.Genre = new List<GenreClass>();
+            mappedMovie.Person = new List<Person>();
+            foreach (var temp in movieModel.GenreClass)
+            {
+                    GenreClass temp1 = await _genreRepository.GetGenre(temp);
+                     mappedMovie.Genre.Add(temp1);
+            }
+            foreach(var temp in movieModel.Person)
+            {
+                Person temp1 = await _personRepository.GetPerson(temp);
+                mappedMovie.Person.Add(temp1);
+            }
+            await _movieRepository.Create(mappedMovie);
+        }
+        public async Task<List<Movie>> GetAllMovies()
+        {
+            var result = await _movieRepository.GetAllMovies();
+            return result;
+        }
+        public async Task<Movie> GetMovieById(int id)
+        {
+            var result =await _movieRepository.GetMovieById(id);
+            return result;
+        }
+
+        public async Task Update(int id, MovieModel movieModel)
+        {
+            var mappedMovie = new Movie()
+            {
+                Description = movieModel.Description,
+                LengthInMin = movieModel.LengthInMin,
+                Name = movieModel.Name,
+                ReleaseTime = movieModel.ReleaseTime
+            };
+            mappedMovie.Genre = new List<GenreClass>();
+            mappedMovie.Person = new List<Person>();
+            foreach (var temp in movieModel.GenreClass)
+            {
+                GenreClass temp1 = await _genreRepository.GetGenre(temp);
+                mappedMovie.Genre.Add(temp1);
+            }
+            foreach (var temp in movieModel.Person)
+            {
+                Person temp1 = await _personRepository.GetPerson(temp);
+                mappedMovie.Person.Add(temp1);
+            }
+            await _movieRepository.Update(id, mappedMovie);
+        }  
+        
+        public async Task DeleteMovie(int id)
+        {
+            await _movieRepository.DeleteMovie(id);
+        }
+    }
+}
